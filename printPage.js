44f60799e9526b63ced75525c5ab0c8d47d4aabf
cjf39 <60300111+cjf39@users.noreply.github.com>
2021-07-27 13:34:19 -0400

    Update printPage.js

--- printPage.js
+++ printPage.js
@@ -3,7 +3,7 @@ var $ = function(id) {
 	return document.getElementById(id);
 }
 var printPage = function() {
-	window.print();
+    window.print(); 
 }
 window.onload = function() {
 	$("print").onclick = printPage;
